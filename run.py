2ae7b06a54385269fd8fab6a336f78346b875c78
23052015 <stani.ste13@gmail.com>
2022-12-22 22:26:06 +0000

    define menu function and creat basic structure

--- run.py
+++ run.py
@@ -23,11 +23,47 @@ def menu():
     """
     Prints the menu and shows options to choose
     """
-    print("[1] show all cars")
 
-    choice_str = int(input("Enter your data here: "))
+    while True:
+        print("1. show all cars")
+        print("2. Mark cars for sell, rent, service")
+        print("3. Filter by model, category/plate/number/code/transmission")
+        print("4. Update fleet")
+        print("5. Exit")
 
 
+        choice = int(input("Choose option: "))
+        if choice == 1:
+            show_all_cars()
+            break
+        elif choice == 2:
+            mark_specific_cars()
+            break
+        elif choice == 3:
+            filter_cars()
+            break
+        elif choice == 4:
+            update()
+            break
+        else:
+            print("Invalid choice. Choose 1-5")
+
+
+def show_all_cars():
+    print("All cars")
+
+
+
+def mark_specific_cars():
+    print("Specific cars")
+
+
+def filter_cars():
+    print("filter cars")
+
+
+def update():
+    print("update cars")
 
 
 menu()

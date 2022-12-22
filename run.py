2fb254fe4b9468880a65fe385ac4a6a7effa665f
23052015 <stani.ste13@gmail.com>
2022-12-22 14:08:57 +0000

    begin with menu and options for program

--- run.py
+++ run.py
@@ -6,7 +6,6 @@ from google.oauth2.service_account import Credentials
 import datetime 
 
 
-
 SCOPE = [
     "https://www.googleapis.com/auth/spreadsheets",
     "https://www.googleapis.com/auth/drive.file",
@@ -17,33 +16,21 @@ CREDS = Credentials.from_service_account_file('creds.json')
 SCOPED_CREDS = CREDS.with_scopes(SCOPE)
 GSPREAD_CLIENT = gspread.authorize(SCOPED_CREDS)
 SHEET = GSPREAD_CLIENT.open('Fleet')
-
 disposition = SHEET.worksheet('disposition')
-car = disposition.range()
-date = datetime()
 
 
-"""
-def get_reservation_data():
-    
-    Get reservation duration and vehicule code
-    
-    print("Please enter the reservation period and vehicle code")
-    print("Reservaion must have customer name and code 4 letters")
-    print("Example: Johny Cage - 01.01.2022 FWAR")
+def menu():
+    """
+    Prints the menu and shows options to choose
+    """
+    print("[1] show all cars")
 
-    reservation_str = input("Enter your data here: ")
-    
-
-get_reservation_data()
-"""
-"""
-def update_reservation_list():
-    Receives a time span and type to be inserted in in the worksheet
-    df["From"] = df["From"].dt.strftime("%Y-%m-%d %H:%M:%S.%f")
-    df["To"] = df["To"].dt.strftime("%Y-%m-%d %H:%M:%S.%f")
-    disposition.update([df.columns.values.tolist()] + df.values.tolist(), value_input_option='USER_ENTERED')
+    choice_str = int(input("Enter your data here: "))
+    return disposition.get_all_values()
 
 
-update_reservation_list()
-"""
+
+menu()
+
+
+
